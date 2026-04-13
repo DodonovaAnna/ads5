3ce109d6a67e5bd79809be74eeefa28d9f54e8a5
DodonovaAnna <annadodonova9723@gmail.com>
2026-04-13 20:11:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,6 +23,52 @@ std::string infx2pstfx(const std::string& inf) {
         continue;
     }
 
+ if (std::isdigit(symb)) {
+        while (j < inf.length() && std::isdigit(inf[j])) {
+            outS.push_back(inf[j]);
+            j++;
+        }
+        j--;
+        outS.push_back(' ');
+    } else if (symb == '(') {
+        stack.push(symb);
+    } else if (symb == ')') {
+        while (!stack.isEmpty() && stack.top() != '(') {
+            outS.push_back(stack.top());
+            outS.push_back(' ');
+            stack.pop();
+        }
+        if (!stack.isEmpty() && stack.top() == '(') {
+            stack.pop();
+        } else {
+            throw std::runtime_error("Err");
+        }
+    } else if (symb == '+' || symb == '-' || symb == '*' || symb == '/') {
+        while (!stack.isEmpty() && priority(stack.top()) >=
+            priority(symb)) {
+            outS.push_back(stack.top());
+            outS.push_back(' ');
+            stack.pop();
+        }
+        stack.push(symb);
+    } else {
+        throw std::runtime_error(std::string("Err") + symb);
+    }
+  }
+  while (!stack.isEmpty()) {
+    if (stack.top() == '(' || stack.top() == ')') {
+        throw std::runtime_error("Err");
+    }
+    outS.push_back(stack.top());
+    outS.push_back(' ');
+    stack.pop();
+  }
+  if (!outS.empty() && outS.back() == ' ') {
+    outS.pop_back();
+  }
+  return outS;
+}
+
 
 int eval(const std::string& pref) {
   // добавьте код
