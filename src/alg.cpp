48ed697e9ac0eb53bfedfc2f4f32c21a1bbd3aa2
DodonovaAnna <annadodonova9723@gmail.com>
2026-04-13 19:51:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,28 @@
 // Copyright 2025 NNTU-CS
+#include <iostream>
 #include <string>
-#include <map>
+#include <sstream>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int priority(char oper) {
+    switch (oper) {
+    case '+':
+    case '-': return 1;
+    case '*':
+    case '/': return 2;
+    default: return 0;
+    }
 }
+
+std::string infx2pstfx(const std::string& inf) {
+  std::string outS;
+  TStack<char, 100> stack;
+  for (size_t j = 0; j < inf.length(); j++) {
+    char symb = inf[j];
+    if (symb == ' ') {
+        continue;
+    }
+
 
 int eval(const std::string& pref) {
   // добавьте код
